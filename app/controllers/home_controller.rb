e4c59c11303f58eeba9e87f4aa3af6067f147d86
Mark McDonald <mark.mcdonald@gmail.com>
2019-05-17 16:21:18 +0700

    workng images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @facilities  = Facility.all
   end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -43,6 +43,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   version :profile do
     process resize_to_fit: [350, 350]
   end
+  
+  
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
